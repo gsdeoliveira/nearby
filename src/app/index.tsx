16432b525b5c5cae43f1a411a625e92e7bc785a3
Gabriel Sousa <gabriielsousa2002@gmail.com>
2025-01-20 11:55:29 -0300

    teste conta

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native'
 export default function Index() {
   return (
     <View>
-      <Text>Hello React Native!</Text>
+      <Text>Hello React Native teste!</Text>
     </View>
-  )
-}
+  ) 
+} 
